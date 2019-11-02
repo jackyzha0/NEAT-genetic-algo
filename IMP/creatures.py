16c10bc6a112bff4aa4be4d6f711b7da60f89083
Scarlet Nguyen <scarletnguyen@dhcp-128-189-236-108.ubcsecure.wireless.ubc.ca>
2019-11-02 11:34:21 -0700

    Implement pygame and adapt existed classes

--- IMP/creatures.py
+++ IMP/creatures.py
@@ -1,14 +1,16 @@
 # Class definition for creatures
 import math
+import pygame
 
 class Creature():
     def __init__(self, x, y, size):
         self.x = x
         self.y = y
         self.size = size
+        self.colour = (0,102,0)
         self.energy = 100. # starting energy, dies if energy = 0
-        self.facing = 0 # [-pi, pi]
-        self.vel = 0
+        self.angle = 0 # [-pi, pi]
+        self.velocity = 0
         self.dead = False
 
     def tick(self):
@@ -18,13 +20,38 @@ class Creature():
             self.dead = True
 
         # update x,y based on vel and facing
-        dx = math.sin(self.facing) * self.vel
-        dy = math.cos(self.facing) * self.vel
+        dx = math.sin(self.angle) * self.velocity
+        dy = math.cos(self.angle) * self.velocity
         x += dx
         y += dy
 
+    def render(self, screen):
+        #outline
+        pygame.draw.circle(screen, self.colour,
+        (self.x, self.y), self.size, 2)
+        #solid
+        pygame.draw.circle(screen, self.colour,
+        (self.x, self.y), self.size - 5, 0)
+
+
+    def bounce(self, width, height):
+        if self.x > (width - self.size):
+            self.x = 2 * (width - self.size) - self.x
+            self.angle = - self.angle
+        elif self.x < self.size:
+            self.x = 2 * self.size - self.x
+            self.angle = - self.angle
+
+        if self.y > height - self.size:
+            self.y = 2 * (height - self.size) - self.y
+            self.angle = math.pi - self.angle
+        elif self.y < self.size:
+            self.y = 2 * self.size - self.y
+            self.angle = math.pi - self.angle
+
+
     def accel(self, a):
-        self.vel += a
+        self.velocity += a
 
     def turn(self, r):
-        self.facing += r
+        self.angle += r
