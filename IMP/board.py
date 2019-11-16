617277f323430c79353d000c577606eb4e87fbea
jackyzha0 <j.zhao2k19@gmail.com>
2019-11-15 22:24:29 -0800

    updates

--- IMP/board.py
+++ IMP/board.py
@@ -8,8 +8,9 @@ import neat
 import sys
 import time
 
+
 class Board():
-    def __init__(self, w, h, foodspawn = 0.1):
+    def __init__(self, w, h, foodspawn=0.1):
         self.width = w  # dimensions of board
         self.height = h
         self.foodspawnthresh = 1 - foodspawn
@@ -18,14 +19,14 @@ class Board():
         self.creatures = []  # initialized later
         self.GEN_TIMEOUT = 12000  # Constant for generation timeout
         self.FOOD_GEN_DELTA = 5  # Constant for food generation position change
-        self.MAX_FOOD = 15
+        self.MAX_FOOD = 35
         self.FOOD_SCALING = 150  # Constant to multiply food value by
         self.RENDER_SKIP = 1  # render a frame every n frames
         self.DEBUG = True
         self.DEBUG_SLEEP_TIME = 0
 
         pygame.init()
-        self.BACKGROUND_COLOR = (220,220,220)
+        self.BACKGROUND_COLOR = (220, 220, 220)
         pygame.display.set_caption('NATURAL SELECTION SIMULATION')
         self.screen = pygame.display.set_mode((w, h))
         self.screen.fill(self.BACKGROUND_COLOR)
@@ -41,22 +42,20 @@ class Board():
                 if random.random() > self.foodspawnthresh:
 
                     # new x,y position with +/- FOOD_GEN_DELTA constant from current food position
-                    nx = f.x + random.randint(-self.FOOD_GEN_DELTA, self.FOOD_GEN_DELTA)
-                    ny = f.y + random.randint(-self.FOOD_GEN_DELTA, self.FOOD_GEN_DELTA)
+                    nx = f.x + \
+                        random.randint(-self.FOOD_GEN_DELTA,
+                                       self.FOOD_GEN_DELTA)
+                    ny = f.y + \
+                        random.randint(-self.FOOD_GEN_DELTA,
+                                       self.FOOD_GEN_DELTA)
                     x, y = nx, ny
 
                     # if in range, add new food to foodlist
                     if x > 0 and x < self.width and y > 0 and y < self.height:
-                        self.food.append(food.Food(x, y, size=random.randint(3, 5)))  # random size
+                        self.food.append(
+                            food.Food(x, y, size=random.randint(3, 5)))  # random size
 
         for i, c in enumerate(self.creatures):
-            for col_creat in c.collide(self.creatures[i+1:]):
-                if c.size > col_creat.size:          # check which creature is bigger
-                    self.creatures.remove(col_creat)    # kill the smaller one
-                    c.energy += col_creat.size * self.FOOD_SCALING       # increase the bigger one's energy by the smaller one's size
-                else:
-                    self.creatures.pop(i)
-                    col_creat.energy += c.size
             for f in c.collide(self.food):
                 if c.size > f.size:          # check if can eat
                     self.food.remove(f)
@@ -70,47 +69,40 @@ class Board():
 
         self.ticks_total += 1
 
-    def closest(self, x: int, y: int, size: int) -> [int, float, int, float]:
+    def closest(self, x: int, y: int, size: int) -> [int, float]:
         '''
         Find closest predator and prey to x,y, given size.
         Predator is any food/creature that is of strictly greater size
         Prey is any food/creature that is strictly smaller
         '''
-        prey_min_r = self.height * math.sqrt(2) # closest distance doesnt exist, assume infinity
+        prey_min_r = math.sqrt(
+            2)  # closest distance doesnt exist, assume infinity
         prey_min_theta = 0  # angle shouldnt matter when dist is infinity
-        predator_min_r = self.height * math.sqrt(2)
-        predator_min_theta = 0
 
         for c in self.food + self.creatures:  # iterate through all creatures and food
-            r, theta = find_r_theta(x, y, c.x, c.y, self.height)  # calculate distance and angle to object
-            if c.size > size: # check predator
-                if r < predator_min_r:
-                    predator_min_r = r
-                    predator_min_theta = theta
-            if c.size < size: # is prey
+            # calculate distance and angle to object
+            r, theta = find_r_theta(x, y, c.x, c.y, self.height / 2)
+            if c.size < size:  # is prey
                 if r < prey_min_r:
                     prey_min_r = r
                     prey_min_theta = theta
 
         if self.DEBUG:
-            if predator_min_r != self.height * math.sqrt(2):
-                x1 = int(math.sin(predator_min_theta) * predator_min_r * self.height) + int(x)
-                y1 = int(math.cos(predator_min_theta) * predator_min_r * self.height) + int(y)
-                pygame.draw.line(self.screen, (255, 0, 0), (int(x), int(y)), (x1, y1), 1)
-            if prey_min_r != self.height * math.sqrt(2):
-                x2 = int(math.sin(prey_min_theta) * prey_min_r * self.height) + int(x)
-                y2 = int(math.cos(prey_min_theta) * prey_min_r * self.height) + int(y)
-                pygame.draw.line(self.screen, (0, 0, 255), (int(x), int(y)), (x2, y2), 1)
+            if prey_min_r != math.sqrt(2):
+                x2 = int(math.sin(prey_min_theta)
+                         * prey_min_r * self.height / 2) + int(x)
+                y2 = int(math.cos(prey_min_theta)
+                         * prey_min_r * self.height / 2) + int(y)
+                pygame.draw.line(self.screen, (0, 0, 255),
+                                 (int(x), int(y)), (x2, y2), 1)
             pygame.display.flip()
 
-        return self.scale(prey_min_r, prey_min_theta, predator_min_r, predator_min_theta)
+        return self.scale(prey_min_r, prey_min_theta)
 
-    def scale(self, rr, rt, pr, pt):
-        x0 = rr / (self.height * math.sqrt(2))
+    def scale(self, rr, rt):
+        x0 = (1 - rr) * 2
         x1 = rt / (math.pi)
-        x2 = pr / (self.height * math.sqrt(2))
-        x3 = pt / (math.pi)
-        return [x0, x1, x2, x3]
+        return [x0 - 1, x1]
 
     def sim_one_gen(self, genomes: [(int, neat.genome.DefaultGenome)], config: neat.config.Config):
         '''
@@ -141,18 +133,20 @@ class Board():
         # iterate genomes
         for genome_id, genome in genomes:
             genome.fitness = 0  # start with fitness level of 0
-            net = neat.nn.FeedForwardNetwork.create(genome, config)  # from python-neat
+            net = neat.nn.FeedForwardNetwork.create(
+                genome, config)  # from python-neat
             nets.append(net)
-            c = creatures.Creature(x=random.randint(0, self.width), y=random.randint(0,  self.height), size=random.randint(5, 10))  # create creature
+            c = creatures.Creature(x=random.randint(0, self.width), y=random.randint(
+                0,  self.height), size=8)  # create creature
             self.creatures.append(c)
-            self.g_l.append(genome) # append genome to genome list
+            self.g_l.append(genome)  # append genome to genome list
 
         # simulate!
         while self.creatures and (self.ticks_total < self.GEN_TIMEOUT):
             self.board_tick()  # update board
             for i, creature in enumerate(self.creatures):  # update all creatures
                 creature.tick()  # tick creature
-                creature.bounce(self.width, self.height)
+                creature.bounce(self.width, self.height, bounce=False)
 
                 c_i = self.creatures.index(creature)  # index of current
 
@@ -161,12 +155,15 @@ class Board():
                     self.creatures.pop(c_i)
                     nets.pop(c_i)
                 else:
-                    closest = self.closest(creature.x, creature.y, creature.size)
+                    r, theta = self.closest(
+                        creature.x, creature.y, creature.size)
 
-                    input = closest + [creature.angle, creature.velocity]
+                    input = [r, creature.angle - theta, creature.velocity / 5]
 
                     # get neural network output given input
                     net_out = nets[c_i].activate(input)
+                    net_out[1] *= math.pi  # direction mod
+                    # print(input, net_out)
 
                     # modify creature accel based on nn output
                     creature.accel(net_out[0])
@@ -192,12 +189,14 @@ class Board():
 
         pygame.display.flip()
 
+
 def find_r_theta(x1: int, y1: int, x2: int, y2: int, normfact: int) -> (float, float):
     '''
     Small helper function to calculate distance and angle between
     two points (x1,y2) and (x2,y2)
     Theta is calculated relative to the top (0 radians) and the bottom (-pi/pi)
     '''
-    theta = math.atan2((x2 - x1), (y2 - y1 + 1e-8))  # add small epsilon to prevent divide by zero
+    theta = math.atan2((x2 - x1), (y2 - y1 + 1e-8)
+                       )  # add small epsilon to prevent divide by zero
     r = math.hypot((x2-x1), (y2-y1)) / normfact
     return r, theta
--- IMP/creatures.py
+++ IMP/creatures.py
@@ -3,6 +3,7 @@ import math
 import pygame
 import random
 
+
 class Creature():
     def __init__(self, x: int, y: int, size: int):
         '''
@@ -11,7 +12,8 @@ class Creature():
         self.x = x
         self.y = y
         self.size = size  # determines what it can eat, also affects energy consumption
-        self.colour = (random.randrange(254), random.randrange(254), random.randrange(254))
+        self.colour = (random.randrange(
+            254), random.randrange(254), random.randrange(254))
         self.energy = 500.  # starting energy, dies if energy = 0
         self.angle = 0  # [-pi, pi]
         self.velocity = 0
@@ -24,7 +26,8 @@ class Creature():
         Update self properties
         '''
         # consume energy, check if dead
-        self.energy -= abs(self.velocity) + (self.SIZE_ENERGY_RATIO * self.size)
+        self.energy -= abs(self.velocity) + \
+            (self.SIZE_ENERGY_RATIO * self.size)
         if self.energy <= 0:
             self.dead = True
 
@@ -39,42 +42,45 @@ class Creature():
         Blit self onto PyGame surface
         '''
         # print(self.x, self.y, self.energy)
-        pygame.draw.circle(screen, self.colour, (int(self.x), int(self.y)), int(self.size), 0)
+        pygame.draw.circle(screen, self.colour, (int(
+            self.x), int(self.y)), int(self.size), 0)
 
         # if debug:
         x2 = int(math.sin(self.angle) * self.velocity * 5) + int(self.x)
         y2 = int(math.cos(self.angle) * self.velocity * 5) + int(self.y)
-        pygame.draw.line(screen, (0, min((self.energy/500), 1) * 255, 0), (int(self.x), int(self.y)), (x2, y2), 2)
+        pygame.draw.line(screen, (0, min((self.energy/500), 1)
+                                  * 255, 0), (int(self.x), int(self.y)), (x2, y2), 2)
 
-    def bounce(self, width, height):
+    def bounce(self, width, height, bounce=False):
         '''
         Bounce creature off the wall and reflect its angle in the boundary
         given width and height of screen
         '''
-        # if self.x > (width - self.size):
-        #     self.x = 2 * (width - self.size) - self.x
-        #     self.angle = - self.angle
-        # elif self.x < self.size:
-        #     self.x = 2 * self.size - self.x
-        #     self.angle = - self.angle
-        #
-        # if self.y > (height - self.size):
-        #     self.y = 2 * (height - self.size) - self.y
-        #     self.angle = math.pi - self.angle
-        # elif self.y < self.size:
-        #     self.y = 2 * self.size - self.y
-        #     self.angle = math.pi - self.angle
+        if bounce:
+            if self.x > (width - self.size):
+                self.x = 2 * (width - self.size) - self.x
+                self.angle = - self.angle
+            elif self.x < self.size:
+                self.x = 2 * self.size - self.x
+                self.angle = - self.angle
 
-        if self.x > (width - self.size):
-            self.dead = True
-        elif self.x < self.size:
-            self.dead = True
-
-        if self.y > (height - self.size):
-            self.dead = True
-        elif self.y < self.size:
-            self.dead = True
-
+            if self.y > (height - self.size):
+                self.y = 2 * (height - self.size) - self.y
+                self.angle = math.pi - self.angle
+            elif self.y < self.size:
+                self.y = 2 * self.size - self.y
+                self.angle = math.pi - self.angle
+        else:
+            pass
+            # if self.x > (width - self.size):
+            #     self.dead = True
+            # elif self.x < self.size:
+            #     self.dead = True
+            #
+            # if self.y > (height - self.size):
+            #     self.dead = True
+            # elif self.y < self.size:
+            #     self.dead = True
 
     def accel(self, a: float):
         '''
@@ -91,7 +97,6 @@ class Creature():
         '''
         self.angle = r
 
-
     def collide(self, loc):
         '''
         Check if the particle collides with any other creature in the given
@@ -104,10 +109,10 @@ class Creature():
             dx = self.x - c2.x
             dy = self.y - c2.y
 
-            distance = math.hypot(dx, dy) # distance between two creatures
+            distance = math.hypot(dx, dy)  # distance between two creatures
             combined_radius = self.size + c2.size
 
             if distance < combined_radius:
-                collisions.append(c2) # add collided creature's index to list
+                collisions.append(c2)  # add collided creature's index to list
 
         return collisions
